f2f0a91001bd037343544a9a92243af4aea99e75
koki014 <koki.suleymanov@gmail.com>
2021-01-18 16:17:39 +0400

    settings

--- tech/urls.py
+++ tech/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
@@ -23,4 +25,5 @@ urlpatterns = [
     path('', include('main.urls', namespace='main'))
     
 ]
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
