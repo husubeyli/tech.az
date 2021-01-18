cbd462f1d9d917e4effba65d0a8b0f4b169aaa34
koki014 <koki.suleymanov@gmail.com>
2021-01-18 16:20:39 +0400

    settings

--- tech/urls.py
+++ tech/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main'))
     
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
